3143df4f3f9e308df082277f8435fe4a8559d429
glowredman <fredcraft00@gmail.com>
2021-09-03 15:04:27 +0200

    Reload datapacks on FMLServerStartedEvent

--- src/main/java/net/darkhax/openloader/OpenLoader.java
+++ src/main/java/net/darkhax/openloader/OpenLoader.java
@@ -5,20 +5,25 @@ import org.apache.logging.log4j.Logger;
 
 import net.darkhax.openloader.OpenLoaderPackFinder.Type;
 import net.minecraft.client.Minecraft;
+import net.minecraft.server.packs.repository.PackRepository;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.loading.FMLEnvironment;
 import net.minecraftforge.fmlserverevents.FMLServerAboutToStartEvent;
+import net.minecraftforge.fmlserverevents.FMLServerStartedEvent;
 
 @Mod("openloader")
 public class OpenLoader {
 	
 	public static final Logger LOGGER = LogManager.getLogger("OpenLoader");
 	
+	private PackRepository serverPackRepository;
+	
 	public OpenLoader() {
 		
-		MinecraftForge.EVENT_BUS.addListener(this::onServerStart);
+		MinecraftForge.EVENT_BUS.addListener(this::serverAboutToStart);
+		MinecraftForge.EVENT_BUS.addListener(this::serverStarted);
 		
 		if (FMLEnvironment.dist == Dist.CLIENT) {
 			LOGGER.info("Adding resource pack finder.");
@@ -26,9 +31,15 @@ public class OpenLoader {
 		}
 	}
 	
-	private void onServerStart(FMLServerAboutToStartEvent event) {
-		event.getServer().getPackRepository().addPackFinder(new OpenLoaderPackFinder(Type.DATA));
+	private void serverAboutToStart(FMLServerAboutToStartEvent event) {
 		LOGGER.info("Adding data pack finder.");
+		this.serverPackRepository = event.getServer().getPackRepository();
+		this.serverPackRepository.addPackFinder(new OpenLoaderPackFinder(Type.DATA));
+	}
+	
+	private void serverStarted(FMLServerStartedEvent event) {
+		LOGGER.info("Reloading data packs.");
+		this.serverPackRepository.reload();
 	}
 
 }
